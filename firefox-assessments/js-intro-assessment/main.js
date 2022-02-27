0ea0a198d0c88466bb91de1da18fb471331da656
cestrada35 <cestrada0098@gmail.com>
2022-02-26 17:49:13 -0800

    String replacement statements

--- firefox-assessments/js-intro-assessment/main.js
+++ firefox-assessments/js-intro-assessment/main.js
@@ -1,3 +1,4 @@
+//1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
 
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
@@ -8,23 +9,24 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-/*2. RAW TEXT STRINGS
-
-It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
-
-Willy the Goblin
-Big Daddy
-Father Christmas
-
-the soup kitchen
-Disneyland
-the White House
-
-spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away
-
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION*/
+//2. RAW TEXT STRINGS
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let insertX = [
+    "Willy the Goblin",
+    "Big Daddy",
+    "Father Christmas"
+];
+let insertY = [
+    "the soup kitchen",
+    "Disneyland",
+    "the White House"
+];
+let insertZ = [
+    "spontaneously combusted",
+    "melted into a puddle on the sidewalk",
+    "turned into a slug and crawled away"
+];
+//3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
 
@@ -44,3 +46,13 @@ function result() {
   story.textContent = '';
   story.style.visibility = 'visible';
 }
+
+let newStory = storyText;
+let xItem = randomValueFromArray(insertX);
+let yItem = randomValueFromArray(insertY);
+let zItem = randomValueFromArray(insertY);
+
+newStory = newStory.replace(':insertx:',xItem);
+newStory = newStory.replace(':insertx:',xItem);
+newStory = newStory.replace(':inserty:',yItem);
+newStory = newStory.replace(':insertz:',zItem);
