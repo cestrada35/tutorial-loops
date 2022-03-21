b050d48f92261d42d3bb66bf385de1b6dfd05a6c
Christian Estrada <cestrada0098@gmail.com>
2022-03-21 07:15:42 -0700

    Loop through gallery

--- firefox-assessments/js-objects/gallery-start/main.js
+++ firefox-assessments/js-objects/gallery-start/main.js
@@ -5,12 +5,12 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const picArray = ['pic1.jpg','pic2.jpg','pic4.jpg','pic4.jpg','pic5.jpg']
+const picArray = ['pic1.jpg','pic2.jpg','pic4.jpg','pic5.jpg']
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
-
+for (let i = 0; i < picArray.length; i++){
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', picArray[i]);
+  thumbBar.appendChild(newImage);
+}
 /* Wiring up the Darken/Lighten button */
